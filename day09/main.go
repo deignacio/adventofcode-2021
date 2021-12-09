f4ee5983bf63db411091b9d53b9b981f0a4a41cf
David Ignacio <deignacio@gmail.com>
2021-12-08 22:12:22 -0800

    Day 9 part 2

--- day09/main.go
+++ day09/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"adventofcode/utils"
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -14,56 +15,161 @@ func ParseInputs(inputs []string) [][]int {
 		value := make([]int, 0)
 		for j := 0; j < len(input); j++ {
 			val, _ := strconv.Atoi(input[j])
-			value = append(value, val)
+			if val == 9 {
+				value = append(value, -1)
+			} else {
+				value = append(value, 0)
+			}
 		}
 		values = append(values, value)
 	}
 	return values
 }
 
-func IsLow(depths [][]int, x int, y int) bool {
-	current := depths[x][y]
-	if x > 0 {
-		if depths[x-1][y] <= current {
-			return false
-		}
+func PaintBucket(depths [][]int, x int, y int, color int) int {
+	if x < 0 {
+		return 0
 	}
-	if x + 1 < len(depths) {
-		if depths[x+1][y] <= current {
-			return false
-		}
+	if x >= len(depths) {
+		return 0
 	}
-	if y > 0 {
-		if depths[x][y-1] <= current {
-			return false
-		}
+	if y < 0 {
+		return 0
 	}
-	if y + 1 < len(depths[x]) {
-		if depths[x][y+1] <= current {
-			return false
-		}
+	if y >= len(depths[x]) {
+		return 0
 	}
-	return true
+	if depths[x][y] != 0 {
+		return 0
+	}
+
+	depths[x][y] = color
+
+	return 1 +
+		PaintUp(depths, x - 1, y, color) +
+		PaintLeft(depths, x, y - 1, color) +
+		PaintRight(depths, x, y + 1, color) +
+		PaintDown(depths, x + 1, y, color)
 }
 
+func PaintUp(depths [][]int, x int, y int, color int) int {
+	if x < 0 {
+		return 0
+	}
+	if x >= len(depths) {
+		return 0
+	}
+	if y < 0 {
+		return 0
+	}
+	if y > len(depths[x]) {
+		return 0
+	}
+	if depths[x][y] != 0 {
+		return 0
+	}
 
+	depths[x][y] = color
+
+	return 1 +
+		PaintUp(depths, x - 1, y, color) +
+		PaintLeft(depths, x, y - 1, color) +
+		PaintRight(depths, x, y + 1, color)
+}
+
+func PaintLeft(depths [][]int, x int, y int, color int) int {
+	if x < 0 {
+		return 0
+	}
+	if x >= len(depths) {
+		return 0
+	}
+	if y < 0 {
+		return 0
+	}
+	if y > len(depths[x]) {
+		return 0
+	}
+	if depths[x][y] != 0 {
+		return 0
+	}
+
+	depths[x][y] = color
+
+	return 1 +
+		PaintUp(depths, x - 1, y, color) +
+		PaintLeft(depths, x, y - 1, color) +
+		PaintDown(depths, x + 1, y, color)
+}
+
+func PaintRight(depths [][]int, x int, y int, color int) int {
+	if x < 0 {
+		return 0
+	}
+	if x >= len(depths) {
+		return 0
+	}
+	if y < 0 {
+		return 0
+	}
+	if y >= len(depths[x]) {
+		return 0
+	}
+	if depths[x][y] != 0 {
+		return 0
+	}
+
+	depths[x][y] = color
+
+	return 1 +
+		PaintUp(depths, x - 1, y, color) +
+		PaintRight(depths, x, y + 1, color) +
+		PaintDown(depths, x + 1, y, color)
+}
+
+func PaintDown(depths [][]int, x int, y int, color int) int {
+	if x < 0 {
+		return 0
+	}
+	if x >= len(depths) {
+		return 0
+	}
+	if y < 0 {
+		return 0
+	}
+	if y >= len(depths[x]) {
+		return 0
+	}
+	if depths[x][y] != 0 {
+		return 0
+	}
+
+	depths[x][y] = color
+
+	return 1 +
+		PaintLeft(depths, x, y - 1, color) +
+		PaintRight(depths, x, y + 1, color) +
+		PaintDown(depths, x + 1, y, color)
+}
 
 func main() {
 	inputs := utils.AsInputList(utils.ReadInput("/Users/dignacio/Documents/adventofcode/day09/input_part_1"))
 	depths := ParseInputs(inputs)
-	lows := make([]int, 0)
+	color := 1
+	basins := make([]int, 0)
 	for i := 0; i < len(depths); i++ {
 		row := depths[i]
 		for j := 0; j < len(row); j++ {
-			if IsLow(depths, i, j) {
-				lows = append(lows, row[j])
+			if depths[i][j] != 0 {
+				continue
 			}
+			size := PaintBucket(depths, i, j, color)
+			basins = append(basins, size)
+			color++
 		}
 	}
-	fmt.Println(lows)
-	sum := 0
-	for i := 0; i < len(lows); i++ {
-		sum += lows[i] + 1
-	}
-	fmt.Println(sum)
+	sort.Ints(basins)
+	fmt.Println(basins)
+	basinCount := len(basins)
+	fmt.Println(basins[basinCount - 1] * basins[basinCount - 2] * basins[basinCount - 3])
 }
